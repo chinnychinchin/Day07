9db1e4fbb8d2e6fe4a249ab8099c6a7ffc70f428
chinyanlim <chinyan77@gmail.com>
2020-11-04 17:20:58 +0800

    added pagination

--- routers/tv_shows_router.js
+++ routers/tv_shows_router.js
@@ -1,8 +1,10 @@
+const { query } = require('express');
 const express = require('express');
 
 
 //SQL queries
-const SQL_GET_ALL_SHOWS = "select * from tv_shows Order by name desc limit 15 ";
+const SQL_GET_ALL_SHOWS = "select * from tv_shows Order by name desc limit ? offset ?";
+const SQL_GET_COUNT_ALL_SHOWS = "select count(*) as total from tv_shows";
 const SQL_GET_SHOW_BY_ID = "select * from tv_shows where tvid = ?";
 
 module.exports = function(p) {
@@ -13,20 +15,28 @@ const pool = p;
 tv_shows_router.get('/', async (req,res) => {
 
     let tv_shows;
+    let total_shows;
+    const pageNum = parseInt(req.query.pageNum) || 1;
+    const offset = parseInt(req.query.offset) || 0;
+    const limit = 8;
     const conn = await pool.getConnection();
     try{
         
-        const [results,_] = await conn.query(SQL_GET_ALL_SHOWS);
+        const [results,_] = await conn.query(SQL_GET_ALL_SHOWS, [limit, offset]);
         tv_shows = results;
+        const [count,__] = await conn.query(SQL_GET_COUNT_ALL_SHOWS);
+        total_shows = count[0].total
+        console.log(total_shows)
+        const numOfPages = total_shows/limit;
         //const tv_show_names = tv_shows.map(show => show['name'])
         //const tv_show_ids = tv_shows.map(show => show['tvid'])
         res.status(200);
         res.type('text/html');
-        res.render('index',{tv_shows})
+        res.render('index',{noShows:!tv_shows.length,tv_shows, prevOffset: Math.max(0,offset-limit), nextOffset: Math.min(total_shows,offset+limit),pageNum, numOfPages, nextPageNum: Math.min(numOfPages, pageNum+1), prevPageNum: Math.max(1, pageNum -1)})
     }catch(e){
         res.status(500);
         res.type('text/html');
-        res.error(`Error: 500 ${e}`);
+        res.send(`Error: 500 ${e}`);
     }finally{
         conn.release();
     }
